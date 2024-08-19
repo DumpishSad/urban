05d17ab76a3d16d2e613b26d7e44b790d5d823e6
DumpishSad <alex30495607@mail.ru>
2024-08-19 13:22:15 +0300

    add practical task variables

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ number_of_tasks = 12
 number_of_hours_spent = 1.5
 course_name = 'Python'
 time_for_one_task = number_of_hours_spent/number_of_tasks
-print(f'Курс:{course_name}, всего задач:{number_of_tasks}, затрачено часов: {number_of_hours_spent}'
-      f', среднее время выполнения {time_for_one_task} часа.')
+print(f'Курс:{course_name}, '
+      f'всего задач:{number_of_tasks}, '
+      f'затрачено часов: {number_of_hours_spent}, '
+      f'среднее время выполнения {time_for_one_task} часа.')
