2098930d5b6ff10d88363bc0b0100ff9fefdb28d
DumpishSad <alex30495607@mail.ru>
2024-09-04 18:43:06 +0300

    fixed 'custom_write'

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ def custom_write(file_name, strings):
     file.close()
     return strings_positions
 
+
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
-    ]
+]
 
 result = custom_write('test.txt', info)
 for elem in result.items():
-  print(elem)
+    print(elem)
