6379458448d5692335ee4398a540373f83b1bf73
DumpishSad <alex30495607@mail.ru>
2024-09-04 18:41:50 +0300

    add task 'custom_write'

---
+++ main.py
@@ -0,0 +1,20 @@
+def custom_write(file_name, strings):
+    file = open(file_name, 'w', encoding='utf-8')
+    strings_positions = {}
+    for i, string in enumerate(strings, start=1):
+        pos = file.tell()
+        file.write(f'{string}\n')
+        strings_positions[(i, pos)] = string
+    file.close()
+    return strings_positions
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
