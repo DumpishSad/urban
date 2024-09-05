41704ec71714d733ea6ec26d9621a64d5352c073
DumpishSad <alex30495607@mail.ru>
2024-09-05 16:35:27 +0300

    fixed task

--- main.py
+++ main.py
@@ -18,3 +18,8 @@ def is_email(message):
         if message[-len(i):] == i:
             return True
     return False
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
