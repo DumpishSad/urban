4966f5df2f417eb341abd6df83cf9dc65cdb45cc
DumpishSad <alex30495607@mail.ru>
2024-09-08 15:00:56 +0300

    add task call at once

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def add_everything_up(a, b):
-    try:
-        return round(a + b, 3)
-    except TypeError:
-        return str(a) + str(b)
+def apply_all_func(int_list, *functions):
+    result = {}
+    for func in functions:
+        result[func.__name__] = func(int_list)
+    return result
 
 
-print(add_everything_up(123.456, 'строка'))
-print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
