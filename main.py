7db84d0f305b18fcb0a142955d3be66a285f504e
DumpishSad <alex30495607@mail.ru>
2024-09-16 15:40:04 +0300

    add task Knight

---
+++ main.py
@@ -0,0 +1,31 @@
+from threading import Thread
+from time import sleep
+
+
+class Knight(Thread):
+    def __init__(self, kname, power):
+        self.kname = kname
+        self.power = power
+        super().__init__()
+
+    def run(self):
+        print(f'{self.kname}, на нас напали!')
+        enemies = 100
+        days = 0
+        while enemies > 0:
+            sleep(1)
+            days += 1
+            enemies -= self.power
+            if enemies < 0:
+                enemies = 0
+            print(f'{self.kname} сражается {days} суток, осталось {enemies} воинов врага.')
+        print(f'{self.kname} одержал победу спустя {days} дней(я)!')
+
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight('Sir Galahad', 20)
+first_knight.start()
+second_knight.start()
+first_knight.join()
+second_knight.join()
+print('Все битвы закончились!')
