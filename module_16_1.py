15924523ca6e82b42873ae0dabe1931adeed8926
DumpishSad <alex30495607@mail.ru>
2024-09-27 15:58:38 +0300

    add task module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -5,20 +5,20 @@ app = FastAPI()
 
 
 @app.get('/')
-async def welcome():
-    return 'Hello World'
+async def welcome() -> str:
+    return 'Главная страница'
 
 
 @app.get('/user/admin')
-async def welcome_admin():
+async def welcome_admin() -> str:
     return 'Вы вошли как администратор'
 
 
 @app.get('/user/{user_id}')
-async def welcome_user_num(user_id):
+async def welcome_user_num(user_id: int) -> str:
     return f'Вы вышли как пользователь № {user_id}'
 
 
 @app.get('/user/{first_name}/{age}')
-async def user_info(first_name, age):
+async def user_info(first_name: str, age: int) -> str:
     return f'Информация о пользователе. Имя {first_name}, Возраст {age}.'
